1757d0be508244c4cafc4950f955af36f8164de9
Dmitriy <fed3375546@yandex.ru>
2020-05-19 23:33:06 +0300

    http server: added response builder and writer

---
+++ src/main/java/net/devstudy/httpserver/io/impl/DefaultHttpResponseBuilder.java
@@ -0,0 +1,54 @@
+package net.devstudy.httpserver.io.impl;
+
+import java.util.Date;
+
+import net.devstudy.httpserver.io.config.HttpResponseBuilder;
+import net.devstudy.httpserver.io.config.HttpServerConfig;
+import net.devstudy.httpserver.io.config.ReadableHttpResponse;
+
+/**
+ * 
+ * @author devstudy
+ * @see http://devstudy.net
+ */
+class DefaultHttpResponseBuilder extends AbstractHttpConfigurableComponent implements HttpResponseBuilder {
+    // Для получения доступа к HttpServerConfig создаем AbstractHttpConfigurableComponent
+	DefaultHttpResponseBuilder(HttpServerConfig httpServerConfig) {
+		super(httpServerConfig);
+	}
+
+	protected ReadableHttpResponse createReadableHttpResponseInstance() {
+		return new DefaultReadableHttpResponse();
+	}
+
+	@Override
+	public ReadableHttpResponse buildNewHttpResponse() {
+		ReadableHttpResponse response = createReadableHttpResponseInstance();
+		response.setHeader("Date", new Date());
+		response.setHeader("Server", httpServerConfig.getServerInfo().getName());
+		response.setHeader("Content-Language", "en");
+		response.setHeader("Connection", "close");
+		response.setHeader("Content-Type", "text/html");// default content type
+		return response;
+	}
+
+	@Override
+	public void prepareHttpResponse(ReadableHttpResponse response, boolean clearBody) {
+		if (response.getStatus() >= 400 && response.isBodyEmpty()) {
+			// TODO
+		}
+		setContentLength(response);
+		if (clearBody) {
+			// необходим для поддержки метода передачи HEAD(без содержимого)
+			clearBody(response);
+		}
+	}
+
+	protected void setContentLength(ReadableHttpResponse response) {
+		response.setHeader("Content-Length", response.getBodyLength());
+	}
+
+	protected void clearBody(ReadableHttpResponse response) {
+		response.setBody("");
+	}
+}
